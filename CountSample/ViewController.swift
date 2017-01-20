5bde653ae9d43ce062a8154d7ebb3aa122e1cb4d
SUZUKIyuka <yuka.suzuki1218@gmail.com>
2017-01-20 22:11:28 +0900

    プラス機能を追加

--- CountSample/ViewController.swift
+++ CountSample/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var number = 0
+        number = number + 1
     }
 
     override func didReceiveMemoryWarning() {
